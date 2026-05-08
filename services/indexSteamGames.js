ac461c0ea277904634004985aaf8153f151e4aa2
ronansky <ronandrost20@gmail.com>
2026-05-08 22:01:42 +0200

    feat: update indexSteamGames to log game names instead of full data response

--- services/indexSteamGames.js
+++ services/indexSteamGames.js
@@ -8,5 +8,6 @@ export async function indexSteamGames(profileUrl) {
 
     const response = await fetch(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${token}&steamid=${steamId}&include_appinfo=true`)
     const data = await response.json()
-    console.log(data)
+    const gameNames = data.response.games.map(game => game.name)
+    console.log(gameNames)
 }
