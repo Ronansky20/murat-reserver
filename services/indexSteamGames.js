78165f020b662bcfb6f04f49eb7280063bab4795
ronansky <ronandrost20@gmail.com>
2026-05-10 02:14:43 +0200

    feat: enhance indexSteamGames to handle game data writing and logging

--- services/indexSteamGames.js
+++ services/indexSteamGames.js
@@ -1,4 +1,5 @@
 import { indexSteamUsers } from "./indexSteamUsers.js";
+import fs from 'fs'
 import 'dotenv/config';
 
 const token = process.env.STEAM_API_KEY;
@@ -9,5 +10,22 @@ export async function indexSteamGames(profileUrl) {
     const response = await fetch(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${token}&steamid=${steamId}&include_appinfo=true`)
     const data = await response.json()
     const gameNames = data.response.games.map(game => game.name)
-    console.log(gameNames)
+
+
+
+    let writeFileContent = {}
+
+    try {
+        writeFileContent = JSON.parse(fs.readFileSync('./data/games.json', 'utf-8'))
+        writeFileContent[steamId] = gameNames
+        fs.writeFileSync('./data/games.json', JSON.stringify(writeFileContent, null, 2))
+        'Content has been written'
+    } catch {
+        const gameList = {
+            [steamId]: gameNames
+        }
+
+        fs.writeFileSync('./data/games.json', JSON.stringify(gameList, null, 2))
+        console.log('The file was empty, the data has been written.')
+    }
 }
